c6a3590975cb5af3f10910fb95f66d41528f834a
Julio Briones <juliobriones@pop-os.localdomain>
2024-06-17 10:27:39 -0600

    Finished book exercise.

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path
-
-from graphene_django.views import GraphQLView
-
-from .schema import schema
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
+    path("", include("books.urls")),
 ]
